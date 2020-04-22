6acb6e037044c9b97eb5b33754383e2de5ad6777
Joseph Steel <joseph.steel.iv@gmail.com>
2020-04-22 18:04:56 -0400

    Day 1
    fetched index.html, and created forst global variables.

--- js/script.js
+++ js/script.js
@@ -7,18 +7,10 @@ FSJS project 2 - List Filter and Pagination
 
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+Added 2 global variables, per instructions this can be done only using 2 variables. The first stores all the list items and the second variable stores the number of li items to be shown per page, which is 10.
 ***/
-
-
-
+const studentList = document.querySelectorAll('li');
+const itemsPerPage = 10;
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
