78e42cb4fdf59b6628f60527b31fca258f19bd50
Joseph Steel <joseph.steel.iv@gmail.com>
2020-04-26 20:16:19 -0400

    going for Exceeds
    started working on exceeds portion of the project

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ function appendPageLinks(list) {
    }
    //added the class of 'active' to the first link
    ul.firstElementChild.firstElementChild.className = 'active';
-   
+
    //this is a for loop to add an eventlistener to each 'a' link
    for (let i = 0; i < ul.childElementCount; i++) {
       let a = document.querySelectorAll('a');
@@ -66,6 +66,67 @@ function appendPageLinks(list) {
    }
    
 }
+
+
+//exceeds expectations portion
+let headerDiv = document.querySelector('.page-header');
+const searchDiv = document.createElement('div');
+searchDiv.ClassName = 'student-search';
+headerDiv.appendChild(searchDiv);
+let form = document.createElement('form');
+searchDiv.appendChild(form);
+const searchTypeInput = document.createElement('input');
+searchTypeInput.placeholder = "Search for students...";
+form.appendChild(searchTypeInput);
+const button = document.createElement('button');
+button.textContent = "Search";
+form.appendChild(button);
+
+//this function searches the studentList for matches to the input and hides and items that dont match.
+function searchNames(searchInput, names) {
+   //console.log(searchInput);
+   //console.log(names);
+   for (let i = 0; i < names.length; i++) {
+      names[i].style.display = 'none';
+      if (searchTypeInput.value.length != 0 && names[i].textContent.toLowerCase().includes(searchTypeInput.value.toLowerCase())) {
+         names[i].style.display = '';
+      }
+      // console.log(nameList);
+   }
+}
+
+
+//created these for the click event handler
+let h3 = document.querySelectorAll('h3');
+let nameList = [];
+let studentListItems = document.querySelectorAll('.student-item');
+let studentsArray = [];
+
+//event listener that calls the searchNames function with the search input and the studentList.
+form.addEventListener('submit', (event) => {
+   event.preventDefault();
+
+   searchNames(searchTypeInput.value, studentList);
+
+   //this loops over all the names in the list and appends them to the nameList
+   for (let i = 0; i < h3.length; i++) {
+      if (h3[i].textContent.toLowerCase().includes(searchTypeInput.value.toLowerCase())) {
+         nameList.push(h3[i].textContent);
+         studentsArray.push(h3[i].parentElement.parentElement);
+      }
+   }
+
+   //created a variable to i can hide all the previous links that were displayed
+   let a = document.querySelectorAll('a');
+   for (let i = 0; i < a.length; i++) {
+      a[i].style.display = 'none';
+   }
+
+   //call the function to append page links at the end of the list using the nameList
+   appendPageLinks(nameList);
+   showPage(studentsArray, 1);
+   console.log('Submit button is functional!');
+});
 
 //here I call the functions to start on the first page
 showPage(studentList, 1);
